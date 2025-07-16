37cdc5a0aaa2b724fdd28eb68e13582f40dd353c
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-07-16 15:03:30 +0530

    login page created

---
+++ src/AppInitilizer/AppInitializer.java
@@ -0,0 +1,13 @@
+package AppInitilizer;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}

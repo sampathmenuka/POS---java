6897e39edf36ad69c5c7662f691a47a73453e4e4
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-07-19 10:44:33 +0530

    2 forms created

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,5 +1,6 @@
-package AppInitilizer;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class AppInitializer extends Application {
@@ -8,6 +9,12 @@ public class AppInitializer extends Application {
     }
 
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(
+                new Scene(
+                        FXMLLoader.load(getClass().getResource("view/LoginForm.fxml"))
+                )
+        );
+        primaryStage.show();
 
     }
 }
--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane container;
+    public TextField txtEmail;
+    public TextField txtPassword;
+
+    public void AccessDashboardOnAction(ActionEvent actionEvent) {
+    }
+
+    public void CreateAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("SignUpForm");
+    }
+
+    private void setUi(String path) throws IOException {
+        Stage stage = (Stage) container.getScene().getWindow();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+path+".fxml")))
+        );
+    }
 }
